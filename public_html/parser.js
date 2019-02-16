7161a483bc0466139e02600b7738d37056949347
Guangrui Chen <gchen41@jhu.edu>
2019-02-16 12:03:54 -0500

    load JSON file

--- public_html/parser.js
+++ public_html/parser.js
@@ -1,24 +1,24 @@
 function parseCourses() {
 
-    let dataFile = require('./whitingSchoolOfEngineering.json');
-    console.log(dataFile[0].Status);
+    var dataFile = JSON.parse(whitingSchoolOfEngineering);
+    console.log(dataFile[0].Title);
 
     console.log("array length is: " + dataFile.length);
 
-    var app = firebase.initializeApp(config);
-    var db = firebase.firestore(app);
-
-// Add a new document in collection "courses"
-    db.collection("courses").doc("whitingSchoolOfEngineering").set({
-        name: "testName",
-        term: "testTerm"
-    })
-        .then(function() {
-            console.log("Document successfully written!");
-        })
-        .catch(function(error) {
-            console.error("Error writing document: ", error);
-        });
+//     var app = firebase.initializeApp(config);
+//     var db = firebase.firestore(app);
+//
+// // Add a new document in collection "courses"
+//     db.collection("courses").doc("whitingSchoolOfEngineering").set({
+//         name: "testName",
+//         term: "testTerm"
+//     })
+//         .then(function() {
+//             console.log("Document successfully written!");
+//         })
+//         .catch(function(error) {
+//             console.error("Error writing document: ", error);
+//         });
 
 }
 
