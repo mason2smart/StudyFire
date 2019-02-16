3e4cce302a51d70ddd0b64c5289880a01df99809
Guangrui Chen <gchen41@jhu.edu>
2019-02-16 12:19:59 -0500

    XMLHttpRequest load json

--- public_html/parser.js
+++ public_html/parser.js
@@ -1,24 +1,36 @@
 function parseCourses() {
+    //
+    // var dataFile = JSON.parse(whitingSchoolOfEngineering);
+    // console.log(dataFile[0].Title);
+    //
+    // console.log("array length is: " + dataFile.length);
 
-    var dataFile = JSON.parse(whitingSchoolOfEngineering);
-    console.log(dataFile[0].Title);
 
-    console.log("array length is: " + dataFile.length);
+    var client = new XMLHttpRequest();
+    client.open('GET', '/whitingSchoolOfEngineering.json');
+    client.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+            console.log("arr length" + myObj.length);
+            console.log(myObj[0].Title);
+        }
+    }
+    client.send();
 
-//     var app = firebase.initializeApp(config);
-//     var db = firebase.firestore(app);
-//
-// // Add a new document in collection "courses"
-//     db.collection("courses").doc("whitingSchoolOfEngineering").set({
-//         name: "testName",
-//         term: "testTerm"
-//     })
-//         .then(function() {
-//             console.log("Document successfully written!");
-//         })
-//         .catch(function(error) {
-//             console.error("Error writing document: ", error);
-//         });
+    var app = firebase.initializeApp(config);
+    var db = firebase.firestore(app);
+
+// Add a new document in collection "courses"
+    db.collection("courses").doc("whitingSchoolOfEngineering").set({
+        name: "testName",
+        term: "testTerm"
+    })
+        .then(function() {
+            console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
 
 }
 
