647e75b0a5b47fc055c8b58b465cec07ffba751c
Guangrui Chen <gchen41@jhu.edu>
2019-02-16 18:59:46 -0500

    check if doc exists before set/update the doc

--- public_html/Dashboard/parse.js
+++ public_html/Dashboard/parse.js
@@ -11,20 +11,38 @@ function parseCourses() {
             var db = firebase.firestore();
             // Add parse each entry in JSON to the database
             for (i = 0; i < courseEntries.length; i++) {
-                db.collection("college").doc("JHU").collection("semester").doc(courseEntries[i].Term).collection("class").doc(courseEntries[i].OfferingName).set({
-                    courseName: courseEntries[i].Title
-                })
-                    .then(function() {
-                        console.log("Document successfully written!" + i);
-                    })
-                    .catch(function(error) {
-                        console.error("Error writing document: ", error);
-                    });
-            }
+                db.collection('college').doc("JHU").collection("semester").doc(courseEntries[i].Term)
+                    .collection("class").doc(courseEntries[i].OfferingName).get().then(function (doc) {
+                    if (doc.exists) {
+                        courseName: courseEntries[i].Title
+                            .then(function () {
+                                console.log("Document successfully updated!" + i);
+                            })
+                            .catch(function (error) {
+                                console.error("Error updating document: ", error);
+                            });
+                    } else {
+                        doc.set({
+                            courseName: courseEntries[i].Title
+                        })
+                            .then(function () {
+                                console.log("Document successfully written!" + i);
+                            })
+                            .catch(function (error) {
+                                console.error("Error writing document: ", error);
+                            });
+                    }
+                    //
+                    // db.collection("college").doc("JHU").collection("semester").doc(courseEntries[i].Term).collection("class").doc(courseEntries[i].OfferingName).set({
+                    //     courseName: courseEntries[i].Title
+                    // })
+
+                });
         }
     }
     client.send();
 }
+
 
 // start parsing
 parseCourses();
