ff8727b43955702a7e65f1c96581012424dc65de
Guangrui Chen <gchen41@jhu.edu>
2019-02-16 17:57:07 -0500

    parsing all course entries to cloud firestone database

--- public_html/Dashboard/parse.js
+++ public_html/Dashboard/parse.js
@@ -1,27 +1,18 @@
 function parseCourses() {
-    //
-    // var dataFile = JSON.parse(whitingSchoolOfEngineering);
-    // console.log(dataFile[0].Title);
-    //
-    // console.log("array length is: " + dataFile.length);
 
+    // loading JSON file
     var client = new XMLHttpRequest();
     client.open('GET', './whitingSchoolOfEngineering.json');
     client.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var myObj = JSON.parse(this.responseText);
-            console.log("testing 5 items \n");
+            var courseEntries = JSON.parse(this.responseText);
 
-
-
+            // a reference to could firestone database
             var db = firebase.firestore();
-
-// Add a new document in collection "courses"
-            for (i = 0; i < 5; i++) {
-                db.collection("college").doc("JHU").collection("semester").doc(myObj[i].Term).collection("class").doc(myObj[i].OfferingName).set({
-                    courseName: myObj[i].Title
-                    // courseNumber: "EN601220",
-                    // courseTerm: "testTerm"
+            // Add parse each entry in JSON to the database
+            for (i = 0; i < courseEntries.length; i++) {
+                db.collection("college").doc("JHU").collection("semester").doc(courseEntries[i].Term).collection("class").doc(courseEntries[i].OfferingName).set({
+                    courseName: courseEntries[i].Title
                 })
                     .then(function() {
                         console.log("Document successfully written!");
@@ -29,42 +20,11 @@ function parseCourses() {
                     .catch(function(error) {
                         console.error("Error writing document: ", error);
                     });
-
             }
-
         }
     }
     client.send();
-
-
-
-
-
-    // db.collection("courses").add({
-    //     name: "testName",
-    //     term: "testTerm"
-    // })
-    //     .then(function(docRef) {
-    //         console.log("Document written with ID: ", docRef.id);
-    //     })
-    //     .catch(function(error) {
-    //         console.error("Error adding document: ", error);
-    //     });
-
-
-
-
 }
 
-// var database = firebase.database();
-//
-// function writeCourseData(courseID, title, term) {
-//     firebase.database().ref('courses/' + courseID).set({
-//         courseNumber: courseID,
-//         courseName: title,
-//         courseTerm: term
-//     });
-// }
-
+// start parsing
 parseCourses();
-//writeCourseData("EN601220", "Intermediate Programming", "Spring2019");
