74d95f0d9b479d7a5b4631d1e0c25fd48496b0bd
Mason Cole <mason.s.cole@hotmail.com>
2019-02-16 10:09:01 -0500

    Profile Pic now pervasive

--- public_html/Profile/profile.js
+++ public_html/Profile/profile.js
@@ -1,101 +1,15 @@
-function consoleLog(name) {
-	console.log(name);
-}
-
-
-var uid;
-var displayName;
-var docPath;
-firebase.auth().onAuthStateChanged(function (user) {
-	if (user) {
-		// User is signed in.
-		displayName = user.displayName;
-		docPath = firestore.collection("users").doc(user.uid);
-		uid = user.uid;
-		email = user.email;
-		var emailVerified = user.emailVerified;
-		var photoURL = user.photoURL;
-		var providerData = user.providerData;
-
-		dispUserInfo(); //display user info on page
-
-		consoleLog("LOGGED IN FIRED AUTH LISTENER");
-		//toast welcome message
-		M.toast({
-			html: "Welcome " + displayName
-		});
-
-	} else {
-		// User is signed out.
-		uid = null;
-		email = null;
-		displayName = null;
-		docPath = null;
-		//window.location.replace('../');
-	}
-});
-
-
-
-
-
-
-
-function logOff(){
-firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}).catch(function(error) {
-  // An error happened.
+function logOff() {
+	firebase.auth().signOut().then(function () {
+		// Sign-out successful.
+	}).catch(function (error) {
+		// An error happened.
 		//materialize toast
-	M.toast({html: error.message});
-});
+		M.toast({
+			html: error.message
+		});
+	});
 }
 
-//Log to local console
-function consoleLog(name){
-  console.log(name);
-}
-
-//------FIRESTORE-------
-var firestore = firebase.firestore();
-
-function dispUserInfo() { //can be called agian to update user info
-	var fName;
-	var lName;
-	var college;
-	var hasProfPic = Boolean(false);
-	firestore.collection('users').doc(uid).get().then(function (doc) {
-		if (doc.exists) {
-			var data = doc.data();
-			fName = data.fName;
-			lName = data.lName;
-			college = data.college;
-			//gender = data.gender; maybe add option later
-			hasProfPic = data.profPic; 
-		}
-		//if (gender == "M") {
-			//gender = "Male";
-		//} else if (gender == "F") {
-			//gender = "Female";
-		//}
-		//uName.innerText = displayName;  //displays configured USERNAME -- Not stored in firestore
-		flName.innerHTML = fName + " " + lName + '  <font class="materialize-red-text">|</font>  ' + college;
-		eMail.innerText = email;
-			if (hasProfPic != null){
-		previewProfPic(); }
-	})
-}
-//display profile pic if exists
-function previewProfPic() {
-	var storageRef = firebase.storage().ref(uid + '/images/'); 
-	var profpicRef = storageRef.child('profPic');
-	profpicRef.getDownloadURL().then(function(url) {
-	profPic.src = url;
-	console.log('downloaded profpic');
-});}
-
-
-//------------FILE UPLOAD FUNCTION--------------
 $(document).ready(function () {
 	$('#uploadInput input').on('change', function () {
 	consoleLog("INPUT TRIGGER: " + document.querySelector('#upload').files[0].name );
@@ -169,6 +83,84 @@ var storageRef = firebase.storage().ref(uid + '/images/');
 	});
 	//functions.storage.object().onFinalize((object) => {});
 }
+
+$(function () {
+	$("#profpicLink").on('click', function (e) {
+		e.preventDefault();
+		$("#upload:hidden").trigger('click');
+	});
+});
+
+//Log to local console
+function consoleLog(content) {
+	console.log(content);
+}
+
+//------FIRESTORE-------
+var firestore = firebase.firestore();
+
+//------FIREBASE AUTH USER------
+//attaching observer to authentication
+var uid;
+var displayName;
+var docPath;
+var email;
+firebase.auth().onAuthStateChanged(function (user) {
+	if (user) {
+		// User is signed in.
+		displayName = user.displayName;
+		docPath = firestore.collection("users").doc(user.uid);
+		uid = user.uid;
+		email = user.email;
+		var emailVerified = user.emailVerified;
+		var photoURL = user.photoURL;
+		var providerData = user.providerData;
+
+		dispUserInfo(); //display user info on page
+
+		consoleLog("LOGGED IN FIRED AUTH LISTENER");
+		//toast welcome message
+		M.toast({
+			html: "Welcome " + displayName
+		});
+
+	} else {
+		// User is signed out.
+		uid = null;
+		email = null;
+		displayName = null;
+		docPath = null;
+		window.location.replace('../');
+	}
+});
+
+function dispProfPic() { //show profile pic on page load
+	var storageRef = firebase.storage().ref(uid + '/images/'); 
+	var profpicRef = storageRef.child('profPic');
+	profpicRef.getDownloadURL().then(function(url) {
+	profPic.src = url;
+	console.log('downloaded profpic');
+});}
+
+function dispUserInfo() { //can be called agian to update user info
+	var fName;
+	var lName;
+	var hasProfPic;
+	firestore.collection('users').doc(uid).get().then(function (doc) {
+		if (doc.exists) {
+			var data = doc.data();
+			fName = data.fName;
+			lName = data.lName;
+			hasProfPic = data.profPic;
+
+		}
+		flName.innerText = fName + " " + lName;
+		eMail.innerText = email;
+		if (hasProfPic != null){
+		dispProfPic(); }
+	})
+}
+
 function newPicUserInfo() { //store that a profile pic has been uploaded
 			firestore.collection('users').doc(uid).get().then(function (doc) {
 			if (doc.exists) { 
@@ -179,12 +171,9 @@ function newPicUserInfo() { //store that a profile pic has been uploaded
 					consoleLog("new profpic submitted to user profile")
 }}});}
 
-
-$(function () {
-	$("#profpicLink").on('click', function (e) {
-		e.preventDefault();
-		$("#upload:hidden").trigger('click');
-	});
-});
-
-
+function submitUserInfo() {
+		firestore.collection('users').doc(uid).get().then(function (doc) {
+			if (doc.exists) { 
+				//for now
+							window.location.replace('../Dashboard/');
+}});}
