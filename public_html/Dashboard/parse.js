da22d7e28550ea144c091baae0c759f5619862e6
Mason Cole <mason.s.cole@hotmail.com>
2019-02-16 21:34:12 -0500

    WORKING PARSE! OMG SO JANKY

--- public_html/Dashboard/parse.js
+++ public_html/Dashboard/parse.js
@@ -1,29 +1,42 @@
+var termVar;
+var offeringNameVar;
+var courseTitleVar;
+
 function parseCourses() {
 
-    // loading JSON file
-    var client = new XMLHttpRequest();
-    client.open('GET', './current-en.json');
-    client.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var courseEntries = JSON.parse(this.responseText);
+	// loading JSON file
+	var client = new XMLHttpRequest();
+	client.open('GET', './current-en.json');
+	client.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			var courseEntries = JSON.parse(this.responseText);
 
-            // a reference to could firestone database
-            var db = firebase.firestore();
-            // Add parse each entry in JSON to the database
-            for (i = 0; i < courseEntries.length; i++) {
-                db.collection("college").doc("JHU").collection("semester").doc(courseEntries[i].Term).collection("class").doc(courseEntries[i].OfferingName).set({
-                    courseName: courseEntries[i].Title
-                })
-                    .then(function() {
-                        console.log("Document successfully written!");
-                    })
-                    .catch(function(error) {
-                        console.error("Error writing document: ", error);
-                    });
-            }
-        }
-    }
-    client.send();
+			// a reference to could firestone database
+			var db = firebase.firestore();
+			// Add parse each entry in JSON to the database
+			for (var i = 0; i < courseEntries.length; i++) {
+				termVar = courseEntries[i].Term.toString();
+				offeringNameVar = courseEntries[i].OfferingName.toString();
+				courseTitleVar = courseEntries[i].Title.toString();
+				var doc = firebase.firestore().collection("college").doc("JHU").collection("semester").doc(termVar).collection("class").doc(offeringNameVar);
+				db.collection("college").doc("JHU").collection("semester").doc(termVar).collection("class").doc(offeringNameVar).get().then(function (doc) {
+					if (doc.exists) {
+						console.log(termVar);
+						return firebase.firestore().collection("college").doc("JHU").collection("semester").doc(termVar).collection("class").doc(offeringNameVar).update({
+							courseName: courseTitleVar
+						});
+					} else {
+						return firebase.firestore().collection("college").doc("JHU").collection("semester").doc(termVar).collection("class").doc(offeringNameVar).set({
+							courseName: courseTitleVar
+						});
+					}
+				}).catch((fail) => {
+					console.log(fail);
+				});
+			}
+		}
+	}
+	client.send();
 }
 
 
